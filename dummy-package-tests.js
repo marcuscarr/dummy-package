e37d91fe59857f2420d953383b7402f79f99b630
Marcus Carr <marcus.carr@gmail.com>
2015-10-07 11:16:15 -0700

    First commit

---
+++ dummy-package-tests.js
@@ -0,0 +1,11 @@
+// Write your tests here!
+// Here is an example.
+// Tinytest.add('example', function (test) {
+//   test.equal(true, true);
+// });
+
+describe('test', function () {
+  it('works', function () {
+    chai.expect(true).to.be.true;
+  });
+});
