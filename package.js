f86f6df3eb7a3391ff203e01602f01bebd3e6e40
Marcus Carr <marcus.carr@gmail.com>
2015-10-07 20:57:25 -0700

    specify versions for `velocity:core` and `mike:mocha-package`

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'dummy-package',
-  version: '0.0.1',
+  version: '0.0.2',
   // Brief, one-line summary of the package.
   summary: '',
   // URL to the Git repository containing the source code for this package.
@@ -12,13 +12,13 @@ Package.describe({
 
 Package.onUse(function(api) {
   api.versionsFrom('1.2.0.2');
-  api.use('ecmascript');
+  api.use('ecmascript@0.1.5');
   api.addFiles('dummy-package.js');
 });
 
 Package.onTest(function(api) {
-  api.use('ecmascript');
-  api.use('mike:mocha-package');
-  api.use('dummy-package');
-  api.addFiles('dummy-package-tests.js');
+  api.use('ecmascript@0.1.5');
+  api.use(['velocity:core@0.10.4', 'mike:mocha-package@0.5.8']);
+  api.use('dummy-package@0.0.2');
+  api.addFiles('dummy-package-tests.js', ['client', 'server']);
 });
